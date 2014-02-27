045ae70888e3401c53386de115db988724aaeff9
luismigm <luismigm@hotmail.com>
2014-02-27 06:59:04 -0500

    estado endgame, más movimientos de naves

---
+++ src/js/endgame.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  function EndGame() {
+    this.titleTxt = null;
+    this.startTxt = null;
+  }
+
+  EndGame.prototype = {
+
+    create: function () {
+      var x = this.game.width / 2
+        , y = this.game.height / 2;
+
+
+      this.high_score_background = this.add.sprite(0, 0, 'high_score_background');
+
+      this.titleTxt = this.add.bitmapText(x, y, 'EndGame', {font: '16px minecraftia', align: 'center'});
+      this.titleTxt.anchor.setTo(0.5, 0.5);
+
+      this.myButton = this.add.button(this.world.centerX - 95, 400, 'return_button', function() { this.game.state.start('menu') }, this);
+    },
+
+    update: function () {
+
+    },
+
+    onDown: function () {
+      this.game.state.start('game');
+    }
+  };
+
+  window['shooter'] = window['shooter'] || {};
+  window['shooter'].EndGame = EndGame;
+
+}());
